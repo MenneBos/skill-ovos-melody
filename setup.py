7d7c7d5d10771d55eaafa6cc3f553003eecc4461
MenneBos <mennebos@gmail.com>
2025-01-23 21:18:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
+import os
 from setuptools import setup
+from os import walk, path
 
 URL = "https://github.com/OpenVoiceOS/skill-ovos-hello-world"
 SKILL_CLAZZ = "HelloWorldSkill"  # needs to match __init__.py class name
@@ -10,6 +11,23 @@ SKILL_AUTHOR, SKILL_NAME = URL.split(".com/")[-1].split("/")
 SKILL_PKG = SKILL_NAME.lower().replace('-', '_')
 PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{SKILL_CLAZZ}'
 # skill_id=package_name:SkillClass
+
+def find_resource_files():
+    resource_base_dirs = ("locale", "ui", "vocab", "dialog", "regex", "skill")
+    base_dir = path.dirname(__file__)
+    package_data = ["*.json"]
+    for res in resource_base_dirs:
+        if path.isdir(path.join(base_dir, res)):
+            for (directory, _, files) in walk(path.join(base_dir, res)):
+                if files:
+                    package_data.append(
+                        path.join(directory.replace(base_dir, "").lstrip('/'),
+                                  '*'))
+    return package_data
+
+
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), "r") as f:
+    long_description = f.read()
 
 setup(
     name='ovos-skill-melody',
