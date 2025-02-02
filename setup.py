a450656f5787fc5844de5edab1c8f80bab6a5267
MenneBos <mennebos@gmail.com>
2025-02-02 21:05:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from os import walk, path
 from os.path import join, dirname
 
-URL = "https://github.com/OpenVoiceOS/skill-ovos-hello-world"
+URL = "https://github.com/OpenVoiceOS/skill-ovos-melody"
 SKILL_CLAZZ = "HelloWorldSkill"  # needs to match __init__.py class name
 PYPI_NAME = "ovos-skill-hello-world"  # pip install PYPI_NAME
 
