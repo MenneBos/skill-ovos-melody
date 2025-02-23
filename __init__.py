b2a4b31c87c6b72fa5e182f3f259eaa2869b632f
MenneBos <mennebos@gmail.com>
2025-02-23 09:52:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from ovos_workshop.skills.ovos import OVOSSkill
 from ovos_bus_client.message import Message
 import os
 import requests
+#import subprocess
 
 class MelodySkill(OVOSSkill):
     def __init__(self):
@@ -16,7 +17,8 @@ class MelodySkill(OVOSSkill):
         self.speak_dialog("PlayMelody")
         self.play_audio("/home/ovos/.local/share/What_Is_It_You_Are_Trying_To_Achieve_Sir.mp3", False) 
         url = f"http://192.168.1.45/api/manager/logic/webhook/Kantoor/?tag=Menne"
-        requests.get(url, params=args).json()
+        data = requests.get(url, params=args)
+        print(data.json())
 
 def create_skill():
     return MelodySkill()
