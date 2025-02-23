62a2b281e7923fff429963981dd2bb70b3b1dd60
MenneBos <mennebos@gmail.com>
2025-02-23 10:25:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ class MelodySkill(OVOSSkill):
         #self.speak_dialog("PlayMelody")
         self.play_audio("/home/ovos/.local/share/What_Is_It_You_Are_Trying_To_Achieve_Sir.mp3", False) 
     
-    def handle_web_request(self)
+    def handle_web_request(self):
         url = f"http://192.168.1.45/api/manager/logic/webhook/kantoor/?tag=Terre"
         data = requests.get(url)
         print(data.json())
