1839184bb251e1380ec25b7b6d4296cd3a138fad
Menne Bos <mennebos@gmail.com>
2025-03-02 00:08:12 +0100

    Update audio file played in MelodySkill to enhance user experience

--- __init__.py
+++ __init__.py
@@ -6,7 +6,6 @@ import requests
 class MelodySkill(OVOSSkill):
     def __init__(self):
         super().__init__("MelodySkill")
-        self.melody_file = os.path.join(os.path.dirname(__file__), "melody.mp3")
 
     def initialize(self):
         self.add_event('mycroft.melody.play', self.handle_play_melody)
@@ -14,13 +13,11 @@ class MelodySkill(OVOSSkill):
 
     def handle_play_melody(self, message: Message):
         # wait=True will block the message bus until the dialog is finished
-        self.speak_dialog("hello.world", wait=True)
+        self.speak_dialog("Here is a melody", wait=True)
         # this will speak the string without translation
-        self.speak("hello english folks")
-        url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=SkyRadio"
-        data = requests.get(url)
-        print(data.json())
-        self.play_audio("/home/ovos/.venvs/ovos/lib/python3.11/site-packages/skill_ovos_melody/soundbytes/As_You_Wish.mp3", False) 
+        self.speak("Listen to the melody")
+
+        self.play_audio("/home/ovos/.venvs/ovos/lib/python3.11/site-packages/skill_ovos_melody/soundbytes/What_Is_It_You_Are_Trying_To_Achieve_Sir.mp3", False) 
 
 def create_skill():
     return MelodySkill()
